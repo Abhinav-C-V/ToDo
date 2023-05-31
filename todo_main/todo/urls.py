8aca4e31dfcea5a5174b33ce8c77681b49d14cfc
Abhinav-C-V <abhinavcv071@gmail.com>
2023-05-31 14:06:02 +0530

    todo app finished

--- todo_main/todo/urls.py
+++ todo_main/todo/urls.py
@@ -2,5 +2,14 @@ from django.urls import path,include
 from.import views
 
 urlpatterns = [
+    # Add Tasks
     path('addTask/',views.addTask,name = 'addTask'),
+    # Mark as done
+    path('mark_as_done/<int:pk>/',views.mark_as_done,name = 'mark_as_done'),
+    # Undo
+    path('undo_task/<int:pk>/',views.undo_task,name = 'undo_task'),
+    # Edit
+    path('edit_task/<int:pk>/',views.edit_task,name='edit_task'),
+    # Delete
+    path('delete_task/<int:pk>/',views.delete_task,name='delete_task'),
 ]
--- todo_main/todo_main/views.py
+++ todo_main/todo_main/views.py
@@ -1,12 +1,12 @@
-from django.http import HttpResponse
 from django.shortcuts import render
 from todo.models import Task
 
 
 def home(request):
     tasks = Task.objects.filter(is_completed=False).order_by('updated_at')
-    print(tasks)
+    completed_tasks = Task.objects.filter(is_completed=True).order_by('updated_at')
     context = {
-        'tasks1':tasks
+        'tasks1':tasks,
+        'completed_tasks1':completed_tasks,
     }
     return render(request, 'home-todo.html',context)
